479beae7fe3eb7a0a157ca18d5dc1de74f386c75
Théo Lefeuvre <theo.lefeuvre@hotmail.fr>
2022-09-11 00:13:48 +0200

    FR/EN + pages navigation

---
+++ assets/js/pages.js
@@ -0,0 +1,13 @@
+function newPage(newUrl){
+
+    document.location.href = newUrl;
+    //alert("FUNCTION")
+}
+function newFRPage(){
+
+    document.location.href = "index.html";
+}
+function newENPage(){
+
+    document.location.href = "indexEN.html";
+}
