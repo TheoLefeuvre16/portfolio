792dd67dbef72704ff881d233153fc8c928b7242
Théo Lefeuvre <theo.lefeuvre@hotmail.fr>
2022-09-11 14:57:04 +0200

    videos + french page

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -3,11 +3,3 @@ function newPage(newUrl){
     document.location.href = newUrl;
     //alert("FUNCTION")
 }
-function newFRPage(){
-
-    document.location.href = "index.html";
-}
-function newENPage(){
-
-    document.location.href = "indexEN.html";
-}
